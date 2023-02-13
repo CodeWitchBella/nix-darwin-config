c4aa6deb5208f5a8356481b359a283bbbec17ea0
Isabella Skorepova <isabella@skorepova.info>
2023-02-13 18:13:37 +0100

    start with nix

---
+++ darwin/configuration.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+
+  services.nix-daemon.enable = true;
+  
+  programs.zsh.enable = true;
+  #nixpkgs.config.allowUnfree = true;
+ 
+  nix = {
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+  
+  environment.systemPackages = [
+    pkgs.curl
+    pkgs.vim
+    pkgs.htop
+    pkgs.jq
+    pkgs.vscodium
+  ];
+
+  users.users.isabella = {
+    name = "Isabella Skorepova";
+    home = "/Users/isabella";
+  };
+
+  system.stateVersion = 4;
+  home-manager.users.isabella = {pkgs,...}: {
+    home.stateVersion = "22.11";
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+      userEmail = "isabella@skorepova.info";
+      userName = "Isabella Skorepova";
+    };
+  };
+}
+
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Isabella's darwin system";
+
+  inputs = {
+    # Package sets
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
+
+    # Environment/system management
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, darwin }: {
+    darwinConfigurations."Isabellas-MacBook-M2" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./darwin/configuration.nix
+      ];
+    };
+  };
+}
